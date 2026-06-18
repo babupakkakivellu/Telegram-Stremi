d33f42e5cff488437eb2db2b65d113d2f85109c8
Karan Adhikari <doc.adhikari@gmail.com>
2026-06-18 11:47:14 +0530

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -7,17 +7,16 @@ class Telegram:
     API_ID = int(getenv("API_ID", "0"))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
+    
+    DATABASE = [db.strip() for db in (getenv("DATABASE") or "").split(",") if db.strip()]
+
     HELPER_BOT_TOKEN = getenv("HELPER_BOT_TOKEN", "")
 
     BASE_URL = getenv("BASE_URL", "").rstrip('/')
     PORT = int(getenv("PORT", "8000"))
 
-    PARALLEL = int(getenv("PARALLEL", "1"))
-    PRE_FETCH = int(getenv("PRE_FETCH", "1"))
-
     AUTH_CHANNEL = [channel.strip() for channel in (getenv("AUTH_CHANNEL") or "").split(",") if channel.strip()]
-    DATABASE = [db.strip() for db in (getenv("DATABASE") or "").split(",") if db.strip()]
-
+    
     TMDB_API = getenv("TMDB_API", "")
 
     UPSTREAM_REPO = getenv("UPSTREAM_REPO", "")
@@ -28,8 +27,8 @@ class Telegram:
     REPLACE_MODE = getenv("REPLACE_MODE", "true").lower() == "true"
     HIDE_CATALOG = getenv("HIDE_CATALOG", "false").lower() == "true"
 
-    ADMIN_USERNAME = getenv("ADMIN_USERNAME", "fyvio")
-    ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "fyvio")
+    ADMIN_USERNAME = getenv("ADMIN_USERNAME", "admin")
+    ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "admin")
     
     SUBSCRIPTION = getenv("SUBSCRIPTION", "false").lower() == "true"
     SUBSCRIPTION_GROUP_ID = int(getenv("SUBSCRIPTION_GROUP_ID", "0"))
@@ -37,6 +36,5 @@ class Telegram:
     APPROVER_IDS = [int(x.strip()) for x in (getenv("APPROVER_IDS") or "").split(",") if x.strip().isdigit()]
 
     PROXY = getenv("Proxy", "false").lower() == "true"
-    PROXY_TYPE = getenv("ProxyType", "HTTPS")
     HTTP_PROXY_URL = getenv("HTTP_Proxy_URL", "")
     SHOW_PROXY_AND_NON_PROXY_BOTH = getenv("SHOW_ProxyAndNonProxyBoth", "false").lower() == "true"
